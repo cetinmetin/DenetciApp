201d2b1a52d140764a2727a3b8d46afc58ec80cc
cetinmetin <metincetin1996@gmail.com>
2021-02-12 22:14:57 +0300

    Question Update Feature Has Been Added for Admin Panel

--- src/screens/AdminDashboard.js
+++ src/screens/AdminDashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Keyboard, } from 'react-native'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
@@ -10,20 +10,24 @@ import { logoutUser } from '../api/auth-api'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import "firebase/firestore";
-import { render } from 'react-dom'
 
 const AdminDashboard = () => {
   const [data, setData] = React.useState({
     questions: [],
-    forms: []
+    forms: [],
+    newQuestions: [],
+    answerMethods: []
   })
   async function getQuestions() {
     try {
       data.questions = []
+      data.answerMethods = []
+      data.forms = []
       var tempQuestions = await firebase.firestore().collection('Questions').get()
-      tempQuestions.docs.map(doc => data.questions.push(doc));
+      tempQuestions.docs.map(doc => data.questions.push(doc.id));
+      tempQuestions.docs.map(doc => data.answerMethods.push(doc.data().Qmethod));
       data.questions.map(data => setData(previousData => ({ ...previousData, questions: previousData.questions.concat(data) })))
-      //console.log(data.questions.length)
+      data.answerMethods.map(data => setData(previousData => ({ ...previousData, answerMethods: previousData.answerMethods.concat(data) })))
       if (data.questions.length != data.forms.length) {
         CallFormCreater()
       }
@@ -31,14 +35,40 @@ const AdminDashboard = () => {
       alert("Sorular Alınırken Hata Oluştu - " + e)
     }
   }
+  async function updateQuestions() {
+    var documentsToBeDeleted = await firebase.firestore().collection('Questions').get()
+    var documentsToBeDeletedArray = []
+    documentsToBeDeleted.docs.map(doc => documentsToBeDeletedArray.push(doc.id));
+    for (let i = 0; i < documentsToBeDeletedArray.length; i++) {
+      firebase.firestore().collection("Questions").doc(documentsToBeDeletedArray[i]).delete()
+    }
+    for (let i = 0; i < data.questions.length; i++) {
+      firebase.firestore().collection("Questions").doc(data.questions[i])
+        .set({
+          Qmethod: "picture"
+        });
+      console.log(data.questions[i])
+    }
+    getQuestions()
+
+  }
+  function questionInputChange(val, index) {
+    data.questions[index] = val
+    setData({
+      ...data,
+      questions: data.questions
+    })
+  }
   function QuestionForm(index) {
     return (
       <TextInput
-        label="soru"
-        returnKeyType="next"
+        //label={"Soru " + (index + 1)}
+        // onSubmitEditing={Keyboard.dismiss}
+        // autoCorrect={false}
+        placeholder={data.questions[index]}
         key={index}
-        value={data.questions[index].id}
-      //onChangeText={(text) => setName({ value: text, index })}
+        //value={data.questions[index]}
+        onChangeText={(text) => questionInputChange(text, index)}
       />
     )
   }
@@ -68,7 +98,7 @@ const AdminDashboard = () => {
         Soru Ekleme ve Düzenleme
     </Paragraph>
       {data.forms}
-      <Button mode="outlined" onPress={logoutUser}>
+      <Button mode="outlined" onPress={updateQuestions}>
         Soru Ekle
     </Button>
       <Button mode="outlined" onPress={logoutUser}>
