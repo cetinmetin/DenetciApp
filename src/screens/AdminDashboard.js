c3de9721ec274b6b1d411a843d136321b8f0f212
cetinmetin <metincetin1996@gmail.com>
2021-02-14 19:45:39 +0300

    Answer Method Update Feature Has Been Added for Admin Panel

--- src/screens/AdminDashboard.js
+++ src/screens/AdminDashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, TouchableOpacity, Keyboard, } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Keyboard, Text } from 'react-native'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
@@ -10,14 +10,17 @@ import { logoutUser } from '../api/auth-api'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import "firebase/firestore";
+import { Card } from 'react-native-paper';
+import Checkbox from 'expo-checkbox';
 
 const AdminDashboard = () => {
   const [data, setData] = React.useState({
     questions: [],
     forms: [],
     newQuestions: [],
-    answerMethods: []
+    answerMethods: [],
   })
+
   async function getQuestions() {
     try {
       data.questions = []
@@ -25,7 +28,7 @@ const AdminDashboard = () => {
       data.forms = []
       var tempQuestions = await firebase.firestore().collection('Questions').get()
       tempQuestions.docs.map(doc => data.questions.push(doc.id));
-      tempQuestions.docs.map(doc => data.answerMethods.push(doc.data().Qmethod));
+      tempQuestions.docs.map(doc => data.answerMethods.push(doc.data()));
       data.questions.map(data => setData(previousData => ({ ...previousData, questions: previousData.questions.concat(data) })))
       data.answerMethods.map(data => setData(previousData => ({ ...previousData, answerMethods: previousData.answerMethods.concat(data) })))
       if (data.questions.length != data.forms.length) {
@@ -36,21 +39,25 @@ const AdminDashboard = () => {
     }
   }
   async function updateQuestions() {
-    var documentsToBeDeleted = await firebase.firestore().collection('Questions').get()
-    var documentsToBeDeletedArray = []
-    documentsToBeDeleted.docs.map(doc => documentsToBeDeletedArray.push(doc.id));
-    for (let i = 0; i < documentsToBeDeletedArray.length; i++) {
-      firebase.firestore().collection("Questions").doc(documentsToBeDeletedArray[i]).delete()
+    try {
+      var documentsToBeDeleted = await firebase.firestore().collection('Questions').get()
+      var documentsToBeDeletedArray = []
+      documentsToBeDeleted.docs.map(doc => documentsToBeDeletedArray.push(doc.id));
+      for (let i = 0; i < documentsToBeDeletedArray.length; i++) {
+        firebase.firestore().collection("Questions").doc(documentsToBeDeletedArray[i]).delete()
+      }
+      for (let i = 0; i < data.questions.length; i++) {
+        firebase.firestore().collection("Questions").doc(data.questions[i])
+          .set({
+            text: data.answerMethods[i].text,
+            photo: data.answerMethods[i].photo
+          });
+        // console.log(data.questions[i])
+      }
+      getQuestions()
+    } catch (e) {
+      alert("Soru Güncelleme Sırasında Hata Oluştu - " + e)
     }
-    for (let i = 0; i < data.questions.length; i++) {
-      firebase.firestore().collection("Questions").doc(data.questions[i])
-        .set({
-          Qmethod: "picture"
-        });
-      console.log(data.questions[i])
-    }
-    getQuestions()
-
   }
   function questionInputChange(val, index) {
     data.questions[index] = val
@@ -61,30 +68,73 @@ const AdminDashboard = () => {
   }
   function QuestionForm(index) {
     return (
-      <TextInput
-        //label={"Soru " + (index + 1)}
-        // onSubmitEditing={Keyboard.dismiss}
-        // autoCorrect={false}
-        placeholder={data.questions[index]}
-        key={index}
-        //value={data.questions[index]}
-        onChangeText={(text) => questionInputChange(text, index)}
-      />
+      <Card style={{ flex: 1, width: "100%", marginTop: "2%" }} key={index}>
+        <Card.Title title={"Soru " + (index + 1)} />
+        <Card.Content>
+          <TextInput
+            //label={"Soru " + (index + 1)}
+            // onSubmitEditing={Keyboard.dismiss}
+            // autoCorrect={false}
+            placeholder={data.questions[index]}
+            key={index}
+            //value={data.questions[index]}
+            onChangeText={(text) => questionInputChange(text, index)}
+          />
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View>
+              <Text>Cevap Seçeneği:</Text>
+            </View>
+            <View>
+              <Checkbox
+                value={data.answerMethods[index].text}
+                onChange={() => {
+                  data.answerMethods[index].text = !data.answerMethods[index].text;
+                  setData({ ...data, answerMethods: data.answerMethods });
+                  updateQuestions()
+                }}
+                key={index}
+                color={data.answerMethods[index].text ? '#4630EB' : undefined}
+              />
+            </View>
+            <View>
+              <Text>Metin</Text>
+            </View>
+            <View>
+              <Checkbox
+                value={data.answerMethods[index].photo}
+                onChange={() => {
+                  data.answerMethods[index].photo = !data.answerMethods[index].photo;
+                  setData({ ...data, answerMethods: data.answerMethods });
+                  updateQuestions()
+                }}
+                key={index}
+                color={data.answerMethods[index].photo ? '#4630EB' : undefined}
+              />
+            </View>
+            <View>
+              <Text>Fotoğraf</Text>
+            </View>
+          </View>
+        </Card.Content>
+      </Card>
     )
   }
   const CallFormCreater = () => {
-    if (data.questions.length > 0 && data.forms.length != data.questions.length)
+    if (data.questions.length > 0 && data.forms.length != data.questions.length) {
+      data.forms = []
       CreateQuestionForm()
+    }
     else
       getQuestions()
   }
   const CreateQuestionForm = () => {
     //console.log(data.questions.length + " createform")
-    data.forms = []
-    for (let i = 0; i < data.questions.length; i++) {
+    for (let i = 0; i < data.questions.length; i++)
       data.forms.push(QuestionForm(i))
-    }
-    data.forms.map(data => setData(previousData => ({ ...previousData, forms: previousData.forms.concat(data) })))
+    setData({
+      ...data,
+      forms: data.forms
+    })
   }
   useEffect(() => {
     getQuestions()
@@ -101,7 +151,7 @@ const AdminDashboard = () => {
       <Button mode="outlined" onPress={updateQuestions}>
         Soru Ekle
     </Button>
-      <Button mode="outlined" onPress={logoutUser}>
+      <Button mode="outlined" onPress={updateQuestions}>
         Soruları Kaydet/Güncelle
     </Button>
       <Button mode="outlined" onPress={logoutUser}>
