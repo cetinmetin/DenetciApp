30e8b5df64bb817b6e5365339b8a98a4f251a923
cetinmetin <metincetin1996@gmail.com>
2021-02-16 17:34:11 +0300

    Tab Navigation Menu Added for Admin and User Panels

--- App.js
+++ App.js
@@ -11,16 +11,78 @@ import {
   LoginScreen,
   RegisterScreen,
   ForgotPasswordScreen,
-  Dashboard,
-  AdminDashboard
+  UserDashboard,
+  AdminDashboard,
+  AdminReportDashboard,
+  UserReportDashboard
 } from './src/screens'
 import { FIREBASE_CONFIG } from './src/core/config'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const Tab = createBottomTabNavigator();
 
 const Stack = createStackNavigator()
 if (!firebase.apps.length) {
   firebase.initializeApp(FIREBASE_CONFIG)
 }
+function AdminTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
+          if (route.name === 'Soru İşlemleri') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Yapılan Raporlamalar') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Soru İşlemleri" component={AdminDashboard} />
+      <Tab.Screen name="Yapılan Raporlamalar" component={AdminReportDashboard} />
+    </Tab.Navigator>
+  );
+}
+function UserTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Sorular') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Yaptığım Raporlamalar') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Sorular" component={UserDashboard} />
+      <Tab.Screen name="Yaptığım Raporlamalar" component={UserReportDashboard} />
+    </Tab.Navigator>
+  );
+}
 const App = () => {
   return (
     <Provider theme={theme}>
@@ -38,8 +100,8 @@ const App = () => {
           <Stack.Screen name="StartScreen" component={StartScreen} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
+          <Stack.Screen name="Dashboard" component={UserTabNavigator} />
+          <Stack.Screen name="AdminDashboard" component={AdminTabNavigator} />
           <Stack.Screen
             name="ForgotPasswordScreen"
             component={ForgotPasswordScreen}
