27c24b264dca6c219d9c1f9715ef01d17ac0e7a4
cetinmetin <metincetin1996@gmail.com>
2021-03-06 20:22:25 +0300

    Icon Settings Files for Audio Recording Screen Has Been Added to the Project

---
+++ src/components/AudioRecordScreen/Icons.tsx
@@ -0,0 +1,55 @@
+import Icon from "./Icon";
+
+export const RECORD_BUTTON = new Icon(
+  require("../../assets/AudioRecordScreenImages/record_button.png"),
+  70,
+  119
+);
+export const RECORDING = new Icon(
+  require("../../assets/AudioRecordScreenImages/record_icon.png"),
+  20,
+  14
+);
+
+export const PLAY_BUTTON = new Icon(
+  require("../../assets/AudioRecordScreenImages/play_button.png"),
+  34,
+  51
+);
+export const PAUSE_BUTTON = new Icon(
+  require("../../assets/AudioRecordScreenImages/pause_button.png"),
+  34,
+  51
+);
+export const STOP_BUTTON = new Icon(
+  require("../../assets/AudioRecordScreenImages/stop_button.png"),
+  22,
+  22
+);
+
+export const MUTED_BUTTON = new Icon(
+  require("../../assets/AudioRecordScreenImages/muted_button.png"),
+  67,
+  58
+);
+export const UNMUTED_BUTTON = new Icon(
+  require("../../assets/AudioRecordScreenImages/unmuted_button.png"),
+  67,
+  58
+);
+
+export const TRACK_1 = new Icon(
+  require("../../assets/AudioRecordScreenImages/track_1.png"),
+  166,
+  5
+);
+export const THUMB_1 = new Icon(
+  require("../../assets/AudioRecordScreenImages/thumb_1.png"),
+  18,
+  19
+);
+export const THUMB_2 = new Icon(
+  require("../../assets/AudioRecordScreenImages/thumb_2.png"),
+  15,
+  19
+);
